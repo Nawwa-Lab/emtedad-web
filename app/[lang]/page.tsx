c342fc34b391dfbc668e6cd65ff05ba815bc73cf
AdhamSelim21 <161260432+AdhamSelim21@users.noreply.github.com>
2026-08-23 20:23:32 +0300

    feat: initialize localized home page with custom theme and font configurations

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -11,5 +11,9 @@ export default async function Home({
 	const { lang } = await params;
 	const dict = await getDictionary(lang);
 
-	return <div><h1 className="text-3xl font-bold underline text-gold">Hello world!</h1></div>;
+	return <div>
+		<h1 className="text-3xl font-bold underline text-gold">Hello world!</h1>
+		<h2 className="font-cairo">hello from cairo</h2>
+		<h3 className="font-alex">hello from بيس</h3>
+		</div>;
 }
